65cd6613c46ac4f95e5beda441b6344e37b6b771
Brad Coffield <BradCoffield@users.noreply.github.com>
2025-01-05 09:26:46 -0700

    working example

--- lib/ai/embeddings.ts
+++ lib/ai/embeddings.ts
@@ -1,20 +1,20 @@
-import { embed, embedMany } from "ai";
-import { openai } from "@ai-sdk/openai";
-import { db } from "../db";
-import { cosineDistance, desc, gt, sql } from "drizzle-orm";
-import { embeddings } from "../db/schema/embeddings";
+import { embed, embedMany } from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { db } from '../db';
+import { cosineDistance, desc, gt, sql } from 'drizzle-orm';
+import { embeddings } from '../db/schema/embeddings';
 
-const embeddingModel = openai.embedding("text-embedding-ada-002");
+const embeddingModel = openai.embedding('text-embedding-ada-002');
 
 const generateChunks = (input: string): string[] => {
   return input
     .trim()
-    .split(".")
-    .filter((i) => i !== "");
+    .split('.')
+    .filter(i => i !== '');
 };
 
 export const generateEmbeddings = async (
-  value: string
+  value: string,
 ): Promise<Array<{ embedding: number[]; content: string }>> => {
   const chunks = generateChunks(value);
   const { embeddings } = await embedMany({
@@ -25,7 +25,7 @@ export const generateEmbeddings = async (
 };
 
 export const generateEmbedding = async (value: string): Promise<number[]> => {
-  const input = value.replaceAll("\\n", " ");
+  const input = value.replaceAll('\\n', ' ');
   const { embedding } = await embed({
     model: embeddingModel,
     value: input,
@@ -35,16 +35,15 @@ export const generateEmbedding = async (value: string): Promise<number[]> => {
 
 export const findRelevantContent = async (userQuery: string) => {
   const userQueryEmbedded = await generateEmbedding(userQuery);
-const similarity = sql<number>`1 - (${cosineDistance(
-  sql`${embeddings.embedding}`,
-  userQueryEmbedded
-)})`;
-// const similarity = sql<number>`1 - (${cosineDistance(sql`${embeddings.embedding}`, userQueryEmbedded)})`;
- const similarGuides = await db
-  .select({ name: embeddings.content, similarity: sql<number>`1 - (${cosineDistance(sql`${embeddings.embedding}`, userQueryEmbedded)})`.as('similarity') })
-  .from(embeddings)
-  .where(gt(sql<number>`similarity`, 0.5))
-  .orderBy((t) => desc(t.similarity))
-  .limit(4); 
+  const similarity = sql<number>`1 - (${cosineDistance(
+    embeddings.embedding,
+    userQueryEmbedded,
+  )})`;
+  const similarGuides = await db
+    .select({ name: embeddings.content, similarity })
+    .from(embeddings)
+    .where(gt(similarity, 0.5))
+    .orderBy(t => desc(t.similarity))
+    .limit(4);
   return similarGuides;
 };
